ac9434c9c2b0639eca2559ea5f9e5773c7cee7e8
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-12-06 08:22:38 +0100

    client
    .h et .cpp
    architecture

---
+++ client.cpp
@@ -0,0 +1,11 @@
+/**
+* File:     client.cpp 
+* Author:   Lucie BOUCQUEMONT
+* Date:     06/12/2019 
+* Summary:  classe client
+*/
+
+#include "client.h"
+
+//Constructors
+Client::Client(){}
---
+++ client.h
@@ -0,0 +1,29 @@
+/** 
+* File:     client.h 
+* Author:   Lucie BOUCQUEMONT
+* Date:     06/12/2019 
+* Summary:  Déclaration de la classe Client
+*/
+
+#ifndef _client_h
+#define _client_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "produit.h"
+
+using namespace std;
+
+class Client{
+
+    public :
+        Client();
+
+    private:
+        string m_prenom;
+        string m_nom;
+        vector<Produit> m_panier;
+}
+
+#endif // _client_h
