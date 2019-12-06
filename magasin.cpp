eaa45892a5add42bbd46731b3e210d9c40a4b075
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2019-12-06 10:50:43 +0100

    Magasin !!!

--- magasin.cpp
+++ magasin.cpp
@@ -13,21 +13,52 @@ Magasin::Magasin()
 
 }
 
-void Magasin::ajoutProduitAuMagasin(Produit p1)
+void Magasin::ajoutProduitAuMagasin(Produit* p1)
 {
-    m_liste_produits.push_back(&p1); 
+    m_liste_produits.push_back(p1); 
 }
 
 void Magasin::affichageProduitDuMagasin()
 {
-    cout << "   Produits présents dans le magasin   " << endl; 
+    cout <<"\n---------------------------------------------------"<< endl;
+    cout << "   Produits presents dans le magasin   " << endl; 
     cout <<"---------------------------------------------------"<< endl; 
-    cout <<"Nom                 Description             Quantité            Prix"<<endl; 
-    for(Produit* p: m_liste_produits)
+    cout <<"Nom \t Description \t\t Quantite \t Prix"<<endl; 
+    for(Produit *p: m_liste_produits)
     {
         if(&p!=nullptr)
         {
-            //&p.getTitre(); 
+            cout << p->getTitre() << "\t" << p->getDescription() << "\t\t" << p->getQuantite()<< "\t" << p->getPrix() << endl;             
+
+        }
+    }
+}
+
+void Magasin::detailProduitDansMagasin(Produit* p1)
+{
+    cout <<"\n---------------------------------------------------"<< endl;
+    cout << "                   Detail du produit  " << endl; 
+    cout <<"---------------------------------------------------"<< endl; 
+
+     if(&p1!=nullptr)
+    {
+        cout << p1->getTitre() << "\t" << p1->getDescription() << "\t\t" << p1->getQuantite()<< "\t" << p1->getPrix() << endl;             
+
+    }
+
+}
+
+
+void Magasin::UpdateQuantiteProduitDansMagasin(Produit* p1, int quantite)
+{
+    if(&p1!=nullptr)
+    {
+        for(Produit* p : m_liste_produits)
+        {
+            if(p==p1)
+            {
+                p->setQuantite(quantite); 
+            }
         }
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 #include "client.h"
 
 int main() {
-    Magasin EasyStore(); 
+    Magasin EasyStore; 
 
     //PRODUIT 
-    Produit p1("PS4",249.99, "Console de jeu Sony", 10);
+    Produit p1("PS4",249.99, "Console de jeu Sony", 10);    
+    Produit p2("Switch",249.99, "Console de jeu Nitendo", 10);
     p1.toString();
 
     //Client
@@ -17,6 +18,17 @@ int main() {
     c1.toStringClient();
     c2.toStringClient();
     c3.toStringClient();
+
+    EasyStore.ajoutProduitAuMagasin(&p1); 
+    EasyStore.ajoutProduitAuMagasin(&p2); 
+    EasyStore.affichageProduitDuMagasin(); 
+
+    cout << "\nAVANT " ; 
+    EasyStore.detailProduitDansMagasin(&p1);
+    EasyStore.UpdateQuantiteProduitDansMagasin(&p1, 11); 
+    
+    cout << "\nAPRES " ; 
+    EasyStore.detailProduitDansMagasin(&p1); 
 
     return 0;
 }
